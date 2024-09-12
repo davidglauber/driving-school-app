06477f43821839a8d4c80aa00beed8ad0602afee
David Glauber <david.gl@logicalcommander.com>
2024-09-12 15:21:55 -0300

    refactor: removing unecessary files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,8 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider theme={theme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="index" />
       </Stack>
     </ThemeProvider>
   );
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Text, View } from "react-native";
 
-export const index = () => {
+export default function index() {
   return (
     <View>
       <Text>index</Text>
     </View>
   );
-};
+}
