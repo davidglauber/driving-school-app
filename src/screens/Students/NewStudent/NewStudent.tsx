1e62fecda1ad75f632e0fdf7440e04565c07d5b0
David Glauber <david.gl@logicalcommander.com>
2024-09-17 15:46:51 -0300

    fix: fixing keyboard of new student screen

--- src/screens/Students/NewStudent/NewStudent.tsx
+++ src/screens/Students/NewStudent/NewStudent.tsx
@@ -10,6 +10,7 @@ import { ViewBox } from "@/src/utils/restyle/ViewBox";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React, { useEffect } from "react";
 import { FieldValues, SubmitHandler, useForm, useWatch } from "react-hook-form";
+import { KeyboardAvoidingView, Platform } from "react-native";
 import { useGetStudentLocation } from "./useGetStudentLocation/useGetStudentLocation";
 
 export const NewStudent = () => {
@@ -39,111 +40,112 @@ export const NewStudent = () => {
   };
 
   return (
-    <ViewBox
-      height={height}
-      bg="white"
-      paddingVertical="xs"
-      paddingHorizontal="l"
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={20}
     >
-      <LogoHeader />
+      <ViewBox bg="white" paddingVertical="xs" paddingHorizontal="l">
+        <LogoHeader />
 
-      <ScrollViewBox
-        contentContainerStyle={{ paddingBottom: spacing.xxl * 1.5 }}
-        showsVerticalScrollIndicator={false}
-      >
-        <CustomTextInput
-          name="cpf"
-          control={control}
-          labelInput="*CPF"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={11}
-        />
-        <ViewBox marginVertical="xs" />
+        <ScrollViewBox
+          contentContainerStyle={{ paddingBottom: spacing.xxl * 1.5 }}
+          showsVerticalScrollIndicator={false}
+        >
+          <CustomTextInput
+            name="cpf"
+            control={control}
+            labelInput="*CPF"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={11}
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="name"
-          control={control}
-          labelInput="*Nome"
-          placeholder="Digite aqui"
-          autoCapitalize="words"
-        />
-        <ViewBox marginVertical="xs" />
+          <CustomTextInput
+            name="name"
+            control={control}
+            labelInput="*Nome"
+            placeholder="Digite aqui"
+            autoCapitalize="words"
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="cep"
-          control={control}
-          labelInput="*CEP"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={8}
-          isLoading={isLoading}
-        />
-        <ViewBox marginVertical="xs" />
-        <CustomTextInput
-          name="fullAddress"
-          control={control}
-          labelInput="Endereço Completo"
-          placeholder="Digite aqui"
-          editable={false}
-        />
+          <CustomTextInput
+            name="cep"
+            control={control}
+            labelInput="*CEP"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={8}
+            isLoading={isLoading}
+          />
+          <ViewBox marginVertical="xs" />
+          <CustomTextInput
+            name="fullAddress"
+            control={control}
+            labelInput="Endereço Completo"
+            placeholder="Digite aqui"
+            editable={false}
+          />
 
-        <CustomDivider />
-        <CustomTextInput
-          name="feelingDriving"
-          control={control}
-          labelInput="*Sentimento do aluno no volante"
-          placeholder="O que o aluno sente quando está dirigindo?"
-        />
-        <ViewBox marginVertical="xs" />
+          <CustomDivider />
+          <CustomTextInput
+            name="feelingDriving"
+            control={control}
+            labelInput="*Sentimento do aluno no volante"
+            placeholder="O que o aluno sente quando está dirigindo?"
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="necessaryClasses"
-          control={control}
-          labelInput="*Aulas necessárias"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={3}
-        />
-        <ViewBox marginVertical="xs" />
+          <CustomTextInput
+            name="necessaryClasses"
+            control={control}
+            labelInput="*Aulas necessárias"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={3}
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="boughtClasses"
-          control={control}
-          labelInput="*Aulas adquiridas"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={3}
-        />
-        <ViewBox marginVertical="xs" />
+          <CustomTextInput
+            name="boughtClasses"
+            control={control}
+            labelInput="*Aulas adquiridas"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={3}
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="psicolocicalEvaluationRequired"
-          control={control}
-          labelInput="*Consultas psicológicas necessárias"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={3}
-        />
-        <ViewBox marginVertical="xs" />
+          <CustomTextInput
+            name="psicolocicalEvaluationRequired"
+            control={control}
+            labelInput="*Consultas psicológicas necessárias"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={3}
+          />
+          <ViewBox marginVertical="xs" />
 
-        <CustomTextInput
-          name="psicolocicalEvaluationAcquired"
-          control={control}
-          labelInput="*Consultas psicológicas adquiridas"
-          placeholder="Digite aqui"
-          keyboardType="number-pad"
-          maxLength={3}
-        />
+          <CustomTextInput
+            name="psicolocicalEvaluationAcquired"
+            control={control}
+            labelInput="*Consultas psicológicas adquiridas"
+            placeholder="Digite aqui"
+            keyboardType="number-pad"
+            maxLength={3}
+          />
 
-        <CustomButton
-          color="red"
-          titleColor="white"
-          marginTop="xl"
-          title="Salvar"
-          onPress={handleSubmit(onSubmit)}
-        />
-      </ScrollViewBox>
-    </ViewBox>
+          <CustomButton
+            color="red"
+            titleColor="white"
+            marginTop="xl"
+            title="Salvar"
+            onPress={handleSubmit(onSubmit)}
+          />
+        </ScrollViewBox>
+      </ViewBox>
+    </KeyboardAvoidingView>
   );
 };
--- src/screens/Students/SeeStudent/SeeStudent.tsx
+++ src/screens/Students/SeeStudent/SeeStudent.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { LogoHeader } from "@/src/components/LogoHeader/LogoHeader";
+import { spacing } from "@/src/theme/spacing";
+import { height } from "@/src/utils/dimensions";
+import { ScrollViewBox } from "@/src/utils/restyle/ScrollViewBox";
+import { TextBox } from "@/src/utils/restyle/TextBox";
+import { ViewBox } from "@/src/utils/restyle/ViewBox";
 import React from "react";
 
 export const SeeStudent = () => {
   return (
-    <View>
-      <Text>SeeStudent</Text>
-    </View>
+    <ViewBox height={height} bg="white" paddingHorizontal="l">
+      <LogoHeader />
+      <ScrollViewBox
+        contentContainerStyle={{ paddingBottom: spacing.xxl * 1.5 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <TextBox>aksdjkj</TextBox>
+      </ScrollViewBox>
+    </ViewBox>
   );
 };
